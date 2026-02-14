dbe2f01541bb6bb3bc4495437b9a34e8a826de13
CodeSandbox <support@codesandbox.io>
2026-02-14 16:11:24 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const cors = require("cors");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("Server is running 🚀");
 });
 
+// MongoDB connection
+mongoose
+  .connect(
+    "mongodb+srv://sanchit221206_db_user:sanchit123@cluster0.8upzzgl.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Routes
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
+// Start server
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
