4af37e5732c5d08025e6fd890d1dc1598754d93f
CodeSandbox <support@codesandbox.io>
2026-02-14 15:31:59 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello CodeSandbox");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
